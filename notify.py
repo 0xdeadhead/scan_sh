2cd7f9acbb58e1e3751f270cce40ad6a1a7e5f8b
da3m0n <da3m0n>
2020-10-07 21:25:43 +0530

    removed tokens

--- notify.py
+++ notify.py
@@ -6,8 +6,8 @@ import argparse
 parser=argparse.ArgumentParser()
 parser.add_argument("--file",help="file to send")
 args=parser.parse_args()
-ids=[1179253094, 941027155]
-bot=Bot(token='1324292465:AAEi9u7QudguR2VGgJj6pwH40P7zsu4Cw_A')
+ids=[]
+bot=Bot(token='')
 
 for id in ids:
     bot.send_document(chat_id=id,document=open(args.file,"rb"))
