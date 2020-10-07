f70229b09bc264dc945280d9e16f3b191eeebf1d
da3m0n <da3m0n>
2020-10-07 20:33:33 +0530

    main.sh using for loop instead of parallel

--- main.sh
+++ main.sh
@@ -2,5 +2,9 @@
 
 wget https://chaos-data.projectdiscovery.io/index.json
 jq ".[] | if .bounty and .count < 300 then .URL else null end" index.json  | grep -v "null" | sed 's/"//g' | parallel -j 10 wget {}
-find .  -name "*.zip"  | parallel -j 10 bash -c "[[ -d {}_dir ]] || mkdir {}_dir ; unzip {} -d {}_dir"
+for zip in *.zip
+do
+dir=$( echo $zip | cut -f 1 -d . )
+[[ -d "${dir}_dir" ]] || mkdir "${dir}_dir"
+unzip $zip -d "${dir}_dir" -o
 find . -maxdepth 1  -type d -name "*_dir" | parallel -j 10 bash scan.sh {} 
