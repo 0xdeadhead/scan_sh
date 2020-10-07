a6e2be6c76196fa62baeb957551975166278b456
da3m0n <da3m0n>
2020-10-07 21:20:33 +0530

    removed scan.sh

--- scan.sh
+++ scan.sh
@@ -4,7 +4,7 @@ cd $1
 for hosts in *.txt
 do
 httpx -l $hosts -threads 100 -o alive.txt -no-color -silent -follow-redirects
-nuclei -t $HOME/nuclei-templates -o "$1_report.txt" -silent -v -l alive.txt
-python3 ../notify.py --file "$1_report.txt"
+nuclei -t $HOME/nuclei-templates -o "$1_report.txt" -v -l alive.txt
+python3 notify.py --file "$1_report.txt"
 done
 cd ..
