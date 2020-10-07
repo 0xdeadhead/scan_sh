3bc0224ecfec9e846e1837f4cccb91ce2e98c55e
da3m0n <da3m0n>
2020-10-07 21:12:49 +0530

    fixed main.sh

--- main.sh
+++ main.sh
@@ -1,10 +1,18 @@
 #! /bin/bash
 
+rm index.json
+rm *.zip
 wget https://chaos-data.projectdiscovery.io/index.json
-jq ".[] | if .bounty and .count < 300 then .URL else null end" index.json  | grep -v "null" | sed 's/"//g' | parallel -j 10 wget {}
-for zip in *.zip
-do
-dir=$( echo $zip | cut -f 1 -d . )
-[[ -d "${dir}_dir" ]] || mkdir "${dir}_dir"
-unzip $zip -d "${dir}_dir" -o
-find . -maxdepth 1  -type d -name "*_dir" | parallel -j 10 bash scan.sh {} 
+jq ".[] | if .bounty and .count < 50 then .URL else null end" index.json | grep -v "null" | sed 's/"//g' | parallel -j 10 wget {}
+for zip in *.zip; do
+    dir=$(echo $zip | cut -f 1 -d .)
+    [[ -d "${dir}_dir" ]] || mkdir "${dir}_dir"
+    unzip -o $zip -d "${dir}_dir"
+    cd "${dir}_dir"
+    for hosts in *.txt; do
+        httpx -l $hosts -threads 100 -o "${hosts}_alive.txt" -no-color -silent -follow-redirects
+        nuclei -t $HOME/nuclei-templates -o "${dir}_report.txt" -v -l "${hosts}_alive.txt"
+        python3 ../notify.py --file "${dir}_report.txt"
+    done
+    cd ..
+done
